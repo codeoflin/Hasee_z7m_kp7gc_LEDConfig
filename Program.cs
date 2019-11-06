00947aed0de1067adf095fd03ea6712ad41ac8be
h@hhh.im <h@hhh.im>
2019-11-06 14:39:11 +0800

    Added some notes

--- Program.cs
+++ Program.cs
@@ -8,11 +8,14 @@ namespace Hasee_z7m_kp7gc_LEDConfig
     {
         static void Main(string[] args)
         {
-			//修改键盘4个颜色区
-            LEDHelper.ModifyColor(1, Color.LightPink);
+            //关闭LED
+            LEDHelper.CloseLED();
+			//修改4个颜色区
+            LEDHelper.ModifyColor(1, Color.Blue);
 			LEDHelper.ModifyColor(2, Color.Blue);
-			LEDHelper.ModifyColor(3, Color.Green);
-			LEDHelper.ModifyColor(4, Color.Red);
+			LEDHelper.ModifyColor(3, Color.Blue);
+			LEDHelper.ModifyColor(4, Color.Aqua);
+            //修改亮度(0~32)
             LEDHelper.ModifyBrightness(32);
             Console.WriteLine("Hello World!");
         }
